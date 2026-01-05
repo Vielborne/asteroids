a1f07ddc84d992f23b894a8e0fd42da5f6d7165e
Vielborne <mbilaldogar07@gmail.com>
2026-01-04 20:10:44 -0600

    Initialize Pygame, create game window, and implement core game loop

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while screen:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
