aa6110f1350a5348c1db35bc794eb0d395849509
Vielborne <mbilaldogar07@gmail.com>
2026-01-04 19:43:36 -0600

    Initial commit: Asteroids game

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+
+if __name__ == "__main__":
+    main()
